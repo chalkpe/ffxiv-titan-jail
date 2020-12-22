35099243c221498822a0fcfec37dd9ee0b159fcf
Chalk <chalkpe@gmail.com>
2020-12-22 19:30:04 +0900

    feat: prevent direct access from README

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+if (document.referrer.includes('README.md')) {
+  window.alert('비정상적인 접근입니다. 설치 방법을 다시 읽어 주세요.')
+  window.history.back()
+}
+
 import('./App.js').then(({ App }) =>
   new App(document.getElementById('app')))
